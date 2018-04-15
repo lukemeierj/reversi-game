dc6ab97bbf8979c9f48ccd0d6f6d5ea2ee3e401f
lukemeierj <lukemeierj@gmail.com>
2018-04-15 19:22:30 -0400

    Add tile and board representation

--- reversi-game/reversi-game/Board.cs
+++ reversi-game/reversi-game/Board.cs
@@ -8,5 +8,18 @@ namespace reversi_game
 {
     class Board
     {
+        public Board(uint size)
+        {
+            board = new Tile[size, size];
+        }
+
+        public void Place(int x, int y, TileColor color)
+        {
+            Tile placement = new Tile(color);
+            placement.Place(x, y);
+            board[x, y] = placement;
+        }
+
+        private Tile[,] board;
     }
 }
---
+++ reversi-game/reversi-game/Tile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversi_game
+{
+    enum TileColor
+    {
+        WHITE=0,
+        BLACK=1,
+    }
+
+    class Tile
+    {
+        public TileColor Flip()
+        {
+            if (color == TileColor.WHITE)
+            {
+                color = TileColor.BLACK;
+            }
+            else
+            {
+                color = TileColor.WHITE;
+            }
+            return color;
+        }
+
+        public Tuple<int, int> Place(int x, int y)
+        {
+            if (placed) throw new System.InvalidOperationException("Tile already placed!");
+            coords = Tuple.Create<int, int>(x, y);
+            placed = true;
+            return coords;
+        }
+
+        public Tile(TileColor color)
+        {
+            this.color = color;
+            coords = Tuple.Create(-1, -1);
+        }
+
+
+        public TileColor color { get; private set; }
+        private bool placed = false;
+        private Tuple<int, int> coords;
+    }
+}

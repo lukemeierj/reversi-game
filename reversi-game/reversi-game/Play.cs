0872b822bd1f5844a7445144a57dede1914c3cf4
Drew Hayward <d.andrew.hayward@gmail.com>
2018-04-17 14:21:14 -0400

    Start Play Class

---
+++ reversi-game/reversi-game/Play.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reversi_game
+{
+    // Stores the affected tiles
+    class Play
+    {
+        public Play(Board board, TileColor color, Tuple<int,int> coords)
+        {
+            this.color = color;
+            this.coords = coords;
+
+            
+        }
+        TileColor color;
+        Tuple<int, int> coords;
+        List<Tile> affectedTiles;
+    }
+}
